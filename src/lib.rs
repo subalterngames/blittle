dab00f47d60efc1317d964d7c1f6231577374343
Esther Alter <subalterngames@gmail.com>
2025-10-14 14:21:39 -0400

    works!

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,77 @@
-mod rect;
 pub mod stride;
 
 use rayon::prelude::*;
-pub use rect::Rect;
 use std::slice::from_raw_parts_mut;
 
-pub fn blit(src: &[u8], dst: &mut [u8], src_width: usize, dst_rect: &Rect, stride: usize) {
-    let src_height = src.len() / src_width;
-    let src_width_stride = src_width * stride;
+pub fn blit(
+    src: &[u8],
+    dst: &mut [u8],
+    src_w: usize,
+    dst_w: usize,
+    x: usize,
+    y: usize,
+    stride: usize,
+) {
+    let src_h = (src.len() / stride) / src_w;
+    let src_w_stride = stride * src_w;
 
-    get_slices(dst, &dst_rect, stride)
-        .into_par_iter()
-        .zip((0..src_height).into_par_iter())
-        .for_each(|(dst_slice, src_y)| {
-            let x = src_width_stride * src_y;
-            dst_slice.copy_from_slice(&src[x..x + src_width_stride])
+    let ptr = dst.as_mut_ptr();
+    (0..src_h)
+        .map(|src_y| unsafe {
+            let dst_index = to_index(x, y + src_y, dst_w, stride);
+            (
+                src_y,
+                from_raw_parts_mut(ptr.offset(dst_index as isize), src_w_stride),
+            )
         })
+        .collect::<Vec<(usize, &mut [u8])>>()
+        .into_par_iter()
+        .for_each(|(src_y, dst_slice)| {
+            let src_index = to_index(0, src_y, src_w, stride);
+            dst_slice.copy_from_slice(&src[src_index..src_index + src_w_stride]);
+        });
 }
 
-fn get_slices<'s>(buffer: &'s mut [u8], rect: &Rect, stride: usize) -> Vec<&'s mut [u8]> {
-    let ptr = buffer.as_mut_ptr();
-    let x = rect.x * stride;
-    let len = rect.w * stride;
-    (rect.y..rect.y + rect.h)
-        .map(|y| unsafe {
-            from_raw_parts_mut(ptr.offset(((x + y * rect.w) * stride) as isize), len)
-        })
-        .collect()
+/// Source: https://ennogames.com/blog/3d-and-2d-coordinates-to-1d-indexes
+const fn to_index(x: usize, y: usize, w: usize, stride: usize) -> usize {
+    (x + y * w) * stride
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::stride::RGB;
+    use std::fs::File;
+    use std::io::BufWriter;
+    use std::path::Path;
+    use std::slice::{from_raw_parts, from_raw_parts_mut};
+
+    #[test]
+    fn test_blit() {
+        const SRC_W: usize = 32;
+        const SRC_H: usize = 17;
+        const DST_W: usize = 64;
+        const DST_H: usize = 64;
+        let src = [[255u8, 0, 0]; SRC_W * SRC_H];
+        let mut dst = [[0u8, 0, 255]; DST_W * DST_H];
+        unsafe {
+            let src_ptr = src.as_ptr().cast::<u8>();
+            let src_len = SRC_W * SRC_H * RGB;
+            let src = from_raw_parts(src_ptr, src_len);
+            let dst_ptr = dst.as_mut_ptr().cast::<u8>();
+            let dst_len = DST_W * DST_H * RGB;
+            let dst = from_raw_parts_mut(dst_ptr, dst_len);
+
+            blit(src, dst, SRC_W, DST_W, 2, 12, RGB);
+
+            let path = Path::new("blit.png");
+            let file = File::create(path).unwrap();
+            let w = BufWriter::new(file);
+            let mut encoder = png::Encoder::new(w, DST_W as u32, DST_H as u32);
+            encoder.set_color(png::ColorType::Rgb);
+            encoder.set_depth(png::BitDepth::Eight);
+            let mut writer = encoder.write_header().unwrap();
+            writer.write_image_data(&dst).unwrap(); // Save
+        }
+    }
 }
