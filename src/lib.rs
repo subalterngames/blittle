bd39fbbf18aa200e3b37963541de1166dc9af9fb
Esther Alter <subalterngames@gmail.com>
2025-10-14 12:04:37 -0400

    unsafe magic

--- src/lib.rs
+++ src/lib.rs
@@ -3,26 +3,28 @@ pub mod stride;
 
 use rayon::prelude::*;
 pub use rect::Rect;
+use std::slice::from_raw_parts_mut;
 
-pub fn blit(src: &[u8], dst: &mut [u8], src_width: usize, mut dst_rect: Rect, stride: usize) {
+pub fn blit(src: &[u8], dst: &mut [u8], src_width: usize, dst_rect: &Rect, stride: usize) {
     let src_height = src.len() / src_width;
-    let dst_y = dst_rect.y;
-    let dst_x = dst_rect.x;
-    let dst_w = dst_rect.w;
-    (0..src_height).into_par_iter().for_each(|y| {
-        let src_x = src_width * y;
-        let dst_y = dst_y + y;
-        let (start, end) = get_line_indices(dst_x, dst_y, dst_w, stride);
-        &mut dst[start..end].copy_from_slice(&src[src_x..src_x + src_width]);
-    })
+    let src_width_stride = src_width * stride;
+
+    get_slices(dst, &dst_rect, stride)
+        .into_par_iter()
+        .zip((0..src_height).into_par_iter())
+        .for_each(|(dst_slice, src_y)| {
+            let x = src_width_stride * src_y;
+            dst_slice.copy_from_slice(&src[x..x + src_width_stride])
+        })
 }
 
-/// Converts `(x, y)` to a start index.
-/// `stride` is the number of bytes per pixel.
-/// For example, an RGB24 pixel (3 channels, 1 byte per channel) has a stride of 3.
-/// See `crate::strides` for some stride constants.
-const fn get_line_indices(x: usize, y: usize, w: usize, stride: usize) -> (usize, usize) {
-    let start = (x + y * w) * stride;
-    let end = start + w * stride;
-    (start, end)
+fn get_slices<'s>(buffer: &'s mut [u8], rect: &Rect, stride: usize) -> Vec<&'s mut [u8]> {
+    let ptr = buffer.as_mut_ptr();
+    let x = rect.x * stride;
+    let len = rect.w * stride;
+    (rect.y..rect.y + rect.h)
+        .map(|y| unsafe {
+            from_raw_parts_mut(ptr.offset(((x + y * rect.w) * stride) as isize), len)
+        })
+        .collect()
 }
--- src/rect.rs
+++ src/rect.rs
@@ -10,61 +10,8 @@ pub struct Rect {
     pub h: usize,
 }
 
-impl Rect {
-    /// Converts `(x, y)` to a start index.
-    /// `stride` is the number of bytes per pixel.
-    /// For example, an RGB24 pixel (3 channels, 1 byte per channel) has a stride of 3.
-    /// See `crate::strides` for some stride constants.
-    pub(crate) const fn get_line_indices(&self, stride: usize) -> (usize, usize) {
-        let start = (self.x + self.y * self.w) * stride;
-        let end = start + self.w * stride;
-        (start, end)
-    }
-}
-
 impl Display for Rect {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "[{}, {}, {}, {}]", self.x, self.y, self.w, self.h)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::stride::*;
-    
-    #[test]
-    fn test_rect_start() {
-        
-        let mut r = Rect {
-            x: 0,
-            y: 0,
-            w: 64,
-            h: 32
-        };
-        let (start, end) = r.get_line_indices(GRAYSCALE);
-        assert_eq!(start, 0);
-        assert_eq!(end, r.w);
-        let (start, end) = r.get_line_indices(RGB);
-        assert_eq!(start, 0);
-        assert_eq!(end, r.w * 3);
-
-        r.x = 1;
-        let (start, end) = r.get_line_indices(GRAYSCALE);
-        assert_eq!(start, 1);
-        assert_eq!(end, r.w + 1);
-        let (start, end) = r.get_line_indices(RGB);
-        assert_eq!(start, 3);
-        assert_eq!(end, (r.w + 1) * 3);
-        
-
-        r.x = 3;
-        r.y = 2;
-        let (start, end) = r.get_line_indices(GRAYSCALE);
-        assert_eq!(start, 131);
-        assert_eq!(end, 131 + r.w);
-        let (start, end) = r.get_line_indices(RGB);
-        assert_eq!(start, 393);
-        assert_eq!(end, 393 + r.w * 3);
     }
 }
