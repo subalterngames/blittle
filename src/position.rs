f0480ffd35a998a884773287ae133859657f5c5f
Esther Alter <subalterngames@gmail.com>
2025-12-02 10:51:09 -0500

    impl from

--- src/position.rs
+++ src/position.rs
@@ -8,6 +8,21 @@ pub struct PositionI {
     pub y: isize,
 }
 
+impl Display for PositionI {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl From<PositionU> for PositionI {
+    fn from(value: PositionU) -> Self {
+        Self {
+            x: value.x.cast_signed(),
+            y: value.y.cast_signed()
+        }
+    }
+}
+
 /// An unsigned `(x, y)` pixel position.
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
@@ -16,14 +31,17 @@ pub struct PositionU {
     pub y: usize,
 }
 
-impl Display for PositionI {
+impl Display for PositionU {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
 }
 
-impl Display for PositionU {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
+impl From<PositionI> for PositionU {
+    fn from(value: PositionI) -> Self {
+        Self {
+            x: value.x.cast_unsigned(),
+            y: value.y.cast_unsigned()
+        }
     }
 }
