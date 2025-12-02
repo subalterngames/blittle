9beeb394d26fa3652b2972f34bdb44b95a3e3479
Esther Alter <subalterngames@gmail.com>
2025-12-02 11:20:34 -0500

    not in place

--- src/overlay.rs
+++ src/overlay.rs
@@ -1,7 +1,10 @@
 use crate::stride::{RGB, RGBA};
 use bytemuck::{cast_slice, cast_slice_mut};
-use glam::Vec4;
+pub use glam::Vec4;
 
+/// Convert a bitmap of RGB8 pixels (1 byte per channel) into a slice of [`Vec4`].
+///
+/// The size of `dst` must be: `src.len() / 3`.
 pub fn rgb8_to_rgba32_in_place(src: &[u8], dst: &mut [Vec4]) {
     cast_slice::<u8, [u8; RGB]>(src)
         .iter()
@@ -14,6 +17,9 @@ pub fn rgb8_to_rgba32_in_place(src: &[u8], dst: &mut [Vec4]) {
         })
 }
 
+/// Convert a bitmap of RGBA8 pixels (1 byte per channel) into a slice of [`Vec4`].
+///
+/// The size of `dst` must be: `src.len() / 4`.
 pub fn rgba8_to_rgba32_in_place(src: &[u8], dst: &mut [Vec4]) {
     cast_slice::<u8, [u8; RGBA]>(src)
         .iter()
@@ -26,6 +32,9 @@ pub fn rgba8_to_rgba32_in_place(src: &[u8], dst: &mut [Vec4]) {
         });
 }
 
+/// Convert a bitmap of RGB32 pixels (4 bytes per channel) into a raw RGB8 byte slice.
+///
+/// The size of `dst` must be: `src.len() * 3`.
 pub fn rgba32_to_rgb8_in_place(src: &[Vec4], dst: &mut [u8]) {
     src.iter()
         .zip(cast_slice_mut::<u8, [u8; RGB]>(dst))
@@ -36,7 +45,10 @@ pub fn rgba32_to_rgb8_in_place(src: &[Vec4], dst: &mut [u8]) {
         })
 }
 
-pub fn rgba32_to_rgb32_in_place(src: &[Vec4], dst: &mut [u8]) {
+/// Convert a bitmap of RGBA32 pixels (4 bytes per channel) into a raw RGBA byte slice.
+///
+/// The size of `dst` must be: `src.len() * 4`.
+pub fn rgba32_to_rgba8_in_place(src: &[Vec4], dst: &mut [u8]) {
     src.iter()
         .zip(cast_slice_mut::<u8, [u8; RGBA]>(dst))
         .for_each(|(src, dst)| {
@@ -45,4 +57,32 @@ pub fn rgba32_to_rgb32_in_place(src: &[Vec4], dst: &mut [u8]) {
             dst[2] = (src.z * 255.) as u8;
             dst[3] = (src.w * 255.) as u8;
         })
+}
+
+/// Convert a bitmap of RGB8 pixels (1 byte per channel) into a slice of [`Vec4`].
+pub fn rgb8_to_rgba32(src: &[u8]) -> Vec<Vec4> {
+    let mut dst = vec![Vec4::default(); src.len() / RGB];
+    rgb8_to_rgba32_in_place(src, &mut dst);
+    dst
+}
+
+/// Convert a bitmap of RGBA8 pixels (1 byte per channel) into a slice of [`Vec4`].
+pub fn rgb8a_to_rgba32(src: &[u8]) -> Vec<Vec4> {
+    let mut dst = vec![Vec4::default(); src.len() / RGBA];
+    rgba8_to_rgba32_in_place(src, &mut dst);
+    dst
+}
+
+/// Convert a bitmap of RGB32 pixels (4 bytes per channel) into a raw RGB8 byte slice.
+pub fn rgba32_to_rgb8(src: &[Vec4]) -> Vec<u8> {
+    let mut dst = vec![0; src.len() * RGB];
+    rgba32_to_rgb8_in_place(src, &mut dst);
+    dst
+}
+
+/// Convert a bitmap of RGBA32 pixels (4 bytes per channel) into a raw RGBA byte slice.
+pub fn rgba32_to_rgba8(src: &[Vec4]) -> Vec<u8> {
+    let mut dst = vec![0; src.len() * RGBA];
+    rgba32_to_rgba8_in_place(src, &mut dst);
+    dst
 }
