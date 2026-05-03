adf612aee46606afa6b62a5c182f77a7b0e098b6
Esther Alter <subalterngames@gmail.com>
2026-05-03 12:55:49 -0400

    Removed unused PixelBlitter

--- src/lock/blitter.rs
+++
@@ -1,7 +0,0 @@
-pub trait PixelBlitter<P> {
-    fn should_blit_pixel(&self, pixel: &P) -> bool;
-
-    fn blit_row<B: AsRef<[P]> + AsMut<[P]>>(&self, top: &[P], bottom: &mut [P]);
-
-    fn blit_pixel(&self, top: P, bottom: &mut P);
-}
